bcf91ba26e48853992b43afdbe7ec20e79a935c4
Jaakko <jaakko.rautapaa@aalto.fi>
2024-11-15 18:24:59 +0200

    Add example code

--- src/includes.hpp
+++ src/includes.hpp
@@ -1,12 +1,17 @@
 /**
- * Convenience file for all includes
+ * @file includes.hpp
+ * @brief Convenience file for all includes
+ * 
  */
 
 #include <box2d/box2d.h>
 #include <SFML/Graphics.hpp>
+#include <TGUI/TGUI.hpp>
+#include <TGUI/Backend/SFML-Graphics.hpp>
 #include <iostream>
 #include "physics/object.hpp"
 #include "physics/object_defs.hpp"
 #include "physics/special_birds.hpp"
 #include "physics/block.hpp"
 #include "physics/pig.hpp"
+#include "UI/ui_elements.hpp"
